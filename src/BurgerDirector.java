942f99a66aea12c5b0933e9c45f2bb253844950d
Zhasmira <zhasmira.mamyrbayeva2007@gmail.com>
2026-09-10 14:52:18 +0500

    added BurgerDirector director class

--- src/BurgerDirector.java
+++ src/BurgerDirector.java
@@ -1,5 +1,5 @@
 public class BurgerDirector {
     public void makeClassicBurger(BurgerBuilder builder) {
-        builder.setBun("Sesame bun").setPatty("Beef patty").setSauce("Ketchup");
+        builder.setBun("Classic bun").setPatty("Beef patty").setSauce("Ketchup");
     }
 }
