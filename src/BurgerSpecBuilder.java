8f5d4d3c2d94cd0ddc273b2aeedb8f7acfd04b93
Zhasmira <zhasmira.mamyrbayeva2007@gmail.com>
2026-09-10 14:25:59 +0500

    added second concreteBuilder BurgerSpecBuilder

---
+++ src/BurgerSpecBuilder.java
@@ -0,0 +1,19 @@
+public class BurgerSpecBuilder implements BurgerBuilder {
+    private final StringBuilder sb =
+            new StringBuilder("=== Burger Spec Sheet ===\n");
+    public BurgerSpecBuilder setBun(String bun) {
+        sb.append("Bun: ").append(bun).append("\n");
+        return this;
+    }
+    public BurgerSpecBuilder setPatty(String patty) {
+        sb.append("Patty: ").append(patty).append("\n");
+        return this;
+    }
+    public BurgerSpecBuilder setSauce(String sauce) {
+        sb.append("Sauce: ").append(sauce);
+        return this;
+    }
+    public String getResult() {
+        return sb.toString();
+    }
+}
