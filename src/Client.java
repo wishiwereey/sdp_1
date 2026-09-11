52e7b5b37a8dab7bf2838914e9f7bccfb875ddbd
Zhasmira <zhasmira.mamyrbayeva2007@gmail.com>
2026-09-11 17:58:36 +0500

    added client for burger builders

--- src/Client.java
+++ src/Client.java
@@ -1,13 +1,20 @@
 public class Client {
     public static void main(String[] args) {
         BurgerDirector director = new BurgerDirector();
-        BurgerObjectBuilder objectBuilder = new BurgerObjectBuilder();
-        director.makeClassicBurger(objectBuilder);
-        Burger classicBurger = objectBuilder.getResult();
-        BurgerSpecBuilder specBuilder = new BurgerSpecBuilder();
-        director.makeVeganBurger(specBuilder);
-        String veganSpec = specBuilder.getResult();
-        System.out.println(veganSpec);
+        BurgerObjectBuilder classicObjBuilder = new BurgerObjectBuilder();
+        director.makeClassicBurger(classicObjBuilder);
+        Burger classicBurger = classicObjBuilder.getResult();
+        BurgerSpecBuilder classicSpecBuilder = new BurgerSpecBuilder();
+        director.makeClassicBurger(classicSpecBuilder);
+        String classicSpec = classicSpecBuilder.getResult();
+        BurgerSpecBuilder veganSpecBuilder = new BurgerSpecBuilder();
+        director.makeVeganBurger(veganSpecBuilder);
+        String veganSpec = veganSpecBuilder.getResult();
+        System.out.println("--- Classic Burger Object ---");
         System.out.println(classicBurger);
+        System.out.println("\n--- Classic Burger Spec ---");
+        System.out.println(classicSpec);
+        System.out.println("\n--- Vegan Burger Spec ---");
+        System.out.println(veganSpec);
     }
 }
